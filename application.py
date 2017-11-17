4d390009fdce9a97ea053605e379a3792eb6b576
Radu Sibechi <radu@digifit.eu>
2017-11-17 12:24:03 +0100

    preprocessing

---
+++ application.py
@@ -0,0 +1,6 @@
+from Preprocess import Preprocess
+
+
+if __name__ == "__main__":
+    preprocess = Preprocess()
+    preprocess.preprocess()
---
+++ constants.py
@@ -0,0 +1,19 @@
+data_folder = 'data/'
+
+# given by project
+h5_file = 'VQA_image_features.h5'
+json_file = 'VQA_img_features2id.json'
+
+# given by project
+a_train_data_file = 'vqa_annotatons_train.gzip'
+q_train_data_file = 'vqa_questions_train.gzip'
+a_test_data_file = 'vqa_annotatons_test.gzip'
+q_test_data_file = 'vqa_questions_test.gzip'
+
+# where the preprocessed data will be saved
+train_data_write_file = 'train_data_processed.csv'
+test_data_write_file = 'val_data_processed.csv'
+
+# where the dictionary will be saved
+idx2word_file = 'idx2word'
+word2idx_file = 'word2idx'
